0e9151d285d74c9de2eb2957bcde4a8ab7caddcb
pierre <pierre@PC01.lan>
2018-04-16 19:29:42 +0200

    big bang

---
+++ tcpclient/labtcpclient.py
@@ -0,0 +1,128 @@
+#!/usr/bin/python3
+# -*- coding: UTF-8 -*-
+
+# labtcpclient.py
+
+# #####################################################################
+# Copyright (C) La Labomedia November 2016
+#
+# This file is part of mylabotools.
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the
+# Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+# #####################################################################
+
+
+# 2017 07 09 modif send pour retour d'erreur si envoi raté
+# 2017 07 24 ajout connect_sock, try sur close
+
+
+import socket
+from time import sleep
+
+
+class LabTcpClient:
+    """Envoi et réception sur le même socket en TCP.
+    Toutes les méthodes sont sans try.
+    """
+
+    def __init__(self, ip, port):
+
+        self.ip = ip
+        self.port = port
+        self.server_address = (ip, port)
+        self.data = None
+        self.sock = None
+        self.create_socket()
+
+    def create_socket(self):
+        """Création du socket sans try, et avec connexion."""
+
+        while not self.sock:
+            self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            self.connect_sock()
+            print("Création du socket client {}".format(self.server_address))
+            sleep(0.1)
+
+    def connect_sock(self):
+        """Connexion de la socket, si ok retoune 1 sinon None"""
+
+        try:
+            self.sock.connect(self.server_address)
+            return 1
+        except:
+            print("Connexion impossible du client sur {}".
+                  format(self.server_address))
+            return None
+
+    def send(self, msg):
+        """Envoi d'un message, avec send, msg doit être encodé avant."""
+
+        # Création d'un socket si besoin
+        if not self.sock:
+            self.create_socket()
+
+        # Envoi
+        try:
+            self.sock.send(msg)
+        except:
+            print("Envoi raté: {}".format(msg))
+            # Nouvelle création d'une socket
+            self.sock.close()
+            self.sock = None
+
+    def reconnect(self):
+        """Reconnexion."""
+
+        self.sock = None
+        self.create_socket()
+
+    def close_sock(self):
+        """Fermeture de la socket."""
+
+        try:
+            self.sock.close()
+        except:
+            print("La socket client est déjà close")
+
+        self.sock = None
+
+    def listen(self):
+        """Retourne les data brutes reçues."""
+
+        raw_data = None
+
+        raw_data = self.sock.recv()
+
+        return raw_data
+
+
+if __name__ == "__main__":
+
+    ip = "192.168.1.12"
+    port = 8000
+
+    clt = LabTcpClient(ip, port)
+
+    int_list = [x for x in range(20)]
+    for num in int_list:
+        num = str(num).encode("utf-8")
+        print(num)
+        clt.send(num)
+        sleep(1)
+        if num == 10:
+            clt = None
+
+    clt.close_sock()
---
+++ tcpclient/main.py
@@ -0,0 +1,193 @@
+#!/usr/bin/env python3
+# -*- coding: UTF-8 -*-
+
+"""
+projet TCPclient
+Send data to ip, port for test
+"""
+
+import os, sys
+from time import sleep
+import threading
+
+import kivy
+kivy.require('1.10.0')
+from kivy.app import App
+from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.properties import StringProperty
+from kivy.clock import Clock
+
+from labtcpclient import LabTcpClient
+
+
+class MainScreen(Screen):
+    """Ecran principal"""
+
+    info = StringProperty()
+    
+    def __init__(self, **kwargs):
+        super(MainScreen, self).__init__(**kwargs)
+
+        print("Initialisation de MainScreen ok")
+        
+        self.config = TCPclientApp.get_running_app().config
+        self.create_client()
+
+        # Boucle
+        freq = int(self.config.get('network', 'freq'))
+        tempo = 1 / freq
+        self.event = Clock.schedule_interval(self.update, tempo)
+        self.num = 0
+        
+    def update(self, dt):
+        self.info = str(self.num)
+        data = str(self.num).encode("utf-8")
+        self.clt.send(data)
+        self.num += 1
+        
+    def create_client(self):
+        self.ip = self.config.get('network', 'tcp_ip')
+        self.port = int(self.config.get('network', 'tcp_port'))
+        print("ip =", self.ip)
+        print("port =", self.port)
+        self.clt = LabTcpClient(self.ip, self.port)
+        print("Création d'un client TCP", self.clt)
+
+    def reset_client(self):
+        self.clt = None
+        self.create_client()
+        print("Client réinitialisé")
+        
+    def reset_clock(self):
+        self.event.cancel()
+        freq = int(self.config.get('network', 'freq'))
+        tempo = 1 / freq
+        self.event = Clock.schedule_interval(self.update, tempo)
+
+    def do_reset(self):
+        self.num = 0
+
+     
+SCREENS = { 0: (MainScreen, "Main")}
+
+
+class TCPclientApp(App):
+    
+    def build(self):
+        """Execute en premier apres run()"""
+
+        # Creation des ecrans
+        self.screen_manager = ScreenManager()
+        for i in range(len(SCREENS)):
+            self.screen_manager.add_widget(SCREENS[i][0](name=SCREENS[i][1]))
+
+        return self.screen_manager
+
+    def on_start(self):
+        """Execute apres build()"""
+        pass
+
+    def build_config(self, config):
+        """Si le fichier *.ini n'existe pas,
+        il est cree avec ces valeurs par defaut.
+        Si il manque seulement des lignes, il ne fait rien !
+        """
+
+        config.setdefaults('network',
+                            { 'tcp_ip': '127.0.0.1',
+                              'tcp_port': '8000',
+                              'freq': '1'})
+
+        config.setdefaults('kivy',
+                            { 'log_level': 'debug',
+                              'log_name': 'tcpclient_%y-%m-%d_%_.txt',
+                              'log_dir': '/log',
+                              'log_enable': '1'})
+
+        config.setdefaults('postproc',
+                            { 'double_tap_time': 250,
+                              'double_tap_distance': 20})
+
+    def build_settings(self, settings):
+        """Construit l'interface de l'ecran Options,
+        pour  le serveur seul, Kivy est par defaut,
+        appele par app.open_settings() dans .kv
+        """
+
+        data =  """[{"type": "title", "title":"Reseau"},
+                            {  "type":    "numeric",
+                                "title":   "Frequence",
+                                "desc":    "Frequence entre 1 et 60 Hz",
+                                "section": "network", 
+                                "key":     "freq"},
+
+                    {"type": "title", "title":"Reseau"},
+                            {   "type":    "string",
+                                "title":   "TCP IP",
+                                "desc":    "TCP IP",
+                                "section": "network", 
+                                "key":     "tcp_ip"},
+                                
+                    {"type": "title", "title":"Reseau"},
+                            {   "type":    "numeric",
+                                "title":   "TCP Port",
+                                "desc":    "TCP Port",
+                                "section": "network", 
+                                "key":     "tcp_port"}
+                    ]
+                """
+
+        # self.config est le config de build_config
+        settings.add_json_panel('TCPclient', self.config, data=data)
+
+    def on_config_change(self, config, section, key, value):
+        """Si modification des options, fonction appelee automatiquement
+        """
+
+        freq = int(self.config.get('network', 'freq'))
+        ip = self.config.get('network', 'tcp_ip')
+        port = int(self.config.get('network', 'tcp_port'))
+        menu = self.screen_manager.get_screen("Main")
+
+        if config is self.config:
+            token = (section, key)
+
+            # If frequency change
+            if token == ('network', 'freq'):
+                print("Nouvelle frequence", freq)
+                menu.reset_clock()
+
+            # If ip change
+            if token == ('network', 'tcp_ip'):
+                print("Nouvelle ip", ip)
+                menu.reset_client()
+
+            # If port change
+            if token == ('network', 'tcp_port'):
+                print("Nouveau port", port )
+                menu.reset_client()
+                
+    def go_mainscreen(self):
+        """Retour au menu principal depuis les autres ecrans."""
+
+        #if touch.is_double_tap:
+        self.screen_manager.current = ("Main")
+
+    def do_reset(self):
+        """reset de self.num"""
+
+        menu = self.screen_manager.get_screen("Main")
+        menu.do_reset()
+        
+    def do_quit(self):
+        print("Je quitte proprement")
+
+        # Kivy
+        TCPclientApp.get_running_app().stop()
+
+        # Extinction de tout
+        os._exit(0)
+
+
+if __name__ == "__main__":
+    TCPclientApp().run()
